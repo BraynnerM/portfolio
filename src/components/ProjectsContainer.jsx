77338feb092e78327dbb1be390fb2cf456b2d113
Braynner Marques <braynnermarques@hotmail.com>
2023-06-25 23:55:28 -0300

    mudança de gif para imagem visando resolver o problema

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,7 @@ import '../styles/components/projectscontainer.sass'
 
 
 const projects = [
-    {id: "github.gif", name: "Busca de Usuários Github", app:"https://braynnerm.github.io/projeto-mundo-real-com-fetch-e-github/", repositorie:"https://github.com/BraynnerM/projeto-mundo-real-com-fetch-e-github", description: "Projeto feito utilizando html, css e javascript, neste projeto é utilizado o metodo fetch para busca em uma api, ao pesquisar um usuário é possivel vermos sua foto, nome de usuário, bio cadastrada, seguidores, dados internos de cada repositorio e alêm disso uma lista com os commits mais recentes."}
+    {id: "github.jpg", name: "Busca de Usuários Github", app:"https://braynnerm.github.io/projeto-mundo-real-com-fetch-e-github/", repositorie:"https://github.com/BraynnerM/projeto-mundo-real-com-fetch-e-github", description: "Projeto feito utilizando html, css e javascript, neste projeto é utilizado o metodo fetch para busca em uma api, ao pesquisar um usuário é possivel vermos sua foto, nome de usuário, bio cadastrada, seguidores, dados internos de cada repositorio e alêm disso uma lista com os commits mais recentes."}
 ]
 
 const ProjectsContainer = () => {
@@ -14,7 +14,7 @@ const ProjectsContainer = () => {
                     <div className="project-main">
                         <div>
                             <a href={project.app} target="_blank" rel="noopener noreferrer">
-                                <img src={`./src/assets/main-img/github.gif`} alt="project-screenshot" />
+                                <img src={`./src/assets/projects-img/${project.id}`} alt="project-screenshot" />
                             </a>
                         </div>
                         <div className='project-info'>
