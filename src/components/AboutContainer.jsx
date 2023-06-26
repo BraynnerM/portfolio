562e4706231e715a866531fa0d377d08c37da301
Braynner Marques <braynnermarques@hotmail.com>
2023-06-26 15:42:37 -0300

    atualização do sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,12 +5,10 @@ const AboutContainer = () => {
         Sou um profissional apaixonado por tecnologia e estou constantemente aprimorando minhas habilidades, buscando estar atualizado com as melhores práticas e tendências do mercado. Atualmente, estou cursando Tecnologia em Análise e Desenvolvimento de Sistemas, o que me proporciona uma base sólida em programação e desenvolvimento de software.
         </p>        
         <p>
-        Além de minha formação acadêmica, atuo como freelancer, desenvolvendo projetos personalizados para clientes. Essa experiência tem me permitido aprimorar minhas habilidades de comunicação, trabalho em equipe e gestão de projetos, ampliando minha capacidade de entregar resultados de qualidade.
+        Além de minha formação acadêmica, atuo como freelancer, desenvolvendo projetos personalizados para clientes. Essa experiência tem me permitido aprimorar minhas habilidades de comunicação, conhecimento técnico e gestão de projetos, ampliando minha capacidade de entregar resultados de qualidade.
         </p>
         <p>
-        Estou determinado a enfrentar novos desafios e agregar valor à equipe. Sou um profissional motivado, ávido por aprender e evoluir constantemente. Tenho habilidade para solucionar problemas complexos e estou comprometido em entregar soluções de excelência.</p>
-        <p>
-        Estou ansioso para contribuir como desenvolvedor web e fazer a diferença, trazendo resultados positivos para sua equipe.
+        Sou um profissional motivado, determinado a enfrentar novos desafios, ávido por aprender e evoluir constantemente. Tenho habilidade para solucionar problemas e estou comprometido em contribuir para entregar as melhores soluções e fazer a diferença, trazendo resultados positivos para a equipe.
         </p>
     </section>
 }
