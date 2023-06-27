733397cea6a77bd80f99faf97b9cf003111958da
Braynner Marques <braynnermarques@hotmail.com>
2023-06-26 22:56:46 -0300

    implementação do botão ver mais projetos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,5 +1,6 @@
-import '../styles/components/projectscontainer.sass'
+import { useState } from 'react';
 
+import '../styles/components/projectscontainer.sass'
 
 const projects = [
     {id: "github.gif", name: "Busca de Usuários Github", app: "https://braynnerm.github.io/projeto-mundo-real-com-fetch-e-github/", repositorie: "https://github.com/BraynnerM/projeto-mundo-real-com-fetch-e-github", description: "Projeto feito utilizando HTML, CSS e JavaScript, neste projeto é utilizado o metodo fetch para busca em uma api, ao pesquisar um usuário é possivel vermos sua foto, nome de usuário, bio cadastrada, seguidores, dados internos de cada repositorio e alêm disso uma lista com os commits mais recentes."},
@@ -19,32 +20,42 @@ const projects = [
     {id: "qrcode.png", name: "Componente de QR-Code", app: "https://braynnerm.github.io/qr-code-component-main/", repositorie: "https://github.com/BraynnerM/qr-code-component-main", description: "Pequeno projeto para fins educacionais, utilizando conceitos basicos de HTML, CSS e responsividade"},
 ]
 
-const ProjectsContainer = () => {
+const ProjectsContainer = () => {    
+
+    const [showMoreProjects, setShowMoreProjects] = useState(4);    
+    
+    const handleShowMoreProjects = () => {
+        event.preventDefault();
+        setShowMoreProjects(prevMoreProjects => prevMoreProjects + 4);
+    }
+
     return <section className="projects-container">
         <h2>Projetos</h2>
         <div className="projects-grid">
-            {projects.map((project) => (
+            {projects.slice(0, showMoreProjects).map((project) => (
                 <div className="project-card" id={project.id} key={project.id}>
-                    <div className="project-main">
-                        <div className="project-image-wrapper">
-                            <a href={project.app} target="_blank" rel="noopener noreferrer">
-                                <img className="project-image" src={`./assets/projects-img/${project.id}`} alt="project-screenshot" />
-                            </a>
-                        </div>
-                        <div className='project-info'>
-                            <h3>{project.name}</h3>
-                            <p>{project.description}</p>                        
-                        </div>
+                <div className="project-main">
+                    <div className="project-image-wrapper">
+                        <a href={project.app} target="_blank" rel="noopener noreferrer">
+                            <img className="project-image" src={`./assets/projects-img/${project.id}`} alt="project-screenshot" />
+                        </a>
                     </div>
-                    <div>
-                        <p>Clique na imagem para ver o APP ou <a href={project.repositorie} target="_blank" rel="noopener noreferrer">clique Aqui</a> para ver o repositório</p>
-                    </div>                     
-                </div>                
-            ))}
+                    <div className='project-info'>
+                        <h3>{project.name}</h3>
+                        <p>{project.description}</p>                        
+                    </div>
+                </div>
+                <div>
+                    <p>Clique na imagem para ver o APP ou <a href={project.repositorie} target="_blank" rel="noopener noreferrer">clique Aqui</a> para ver o repositório</p>
+                </div>                     
+            </div> 
+            ))}                               
         </div>
-        <a href="#" className="btn">
-            Ver Mais Projetos
+        {projects.length > showMoreProjects && (
+        <a className="btn" onClick={handleShowMoreProjects}>
+          Ver Mais Projetos
         </a>
+      )}
     </section>
 }
 
